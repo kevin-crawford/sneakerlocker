e146419be7bba3633add384b272f3340205eebee
Kevin Crawford <kcdouglas92@gmail.com>
2019-01-31 15:12:08 -0800

    modified public inventory css

--- public/publiclocker.js
+++ public/publiclocker.js
@@ -20,9 +20,14 @@ function loadPublicInventory() {
 			if(result.length == 0) {
 				$('#public-inventory').append(`
 			<section role="region" class="shoeinfo-region">
-				<h4> No Shoes Found! Please add a shoe.<h4>
+				<h4> No Shoes Found In This Locker<h4>
 			</section>`)
 			} else {
+				$('#public-inventory').append(`
+				<div class="publiclocker-title">
+					<h2>${username} Inventory</h2>
+				</div>
+				`)
 			for(let i = 0; i < result.length; i++){
 				let shoeNumber = (i + 1);
 			$('#public-inventory').append(
